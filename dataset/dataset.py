f659806f77b40d08aca2fe3a2609ea41875a5e58
iivvy <frraroua@gmail.com>
2024-05-27 12:00:45 +0200

    dataset class defined

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-from torch.utils.data import Subset, Dataset 
+from torch.utils.data import Subset, Dataset,DataLoader
 
 class Kimore(Dataset):
     def __init__(self,data,labels,scores,transform=None):
@@ -8,6 +8,7 @@ class Kimore(Dataset):
         self.data = data
         self.labels = labels
         self.scores = scores
+
     def __len__(self):
         return len(self.data)
     
@@ -17,8 +18,12 @@ class Kimore(Dataset):
         score = self.scores[index]
         if self.transform :
             return torch.tensor(x,dtype=torch.float32), torch.tensor(label,dtype=torch.float32), torch.tensor(score,dtype= torch.float32)
-def load_data(root_dir):
+
+
+def load_data(root_dir,batch_size,shuffle):
     data = np.load(root_dir+'data.npy')
     labels = np.load(root_dir+'labels.npy')
     scores = np.load(root_dir+'scores.npy')
-    return data,labels,scores
+    dataset = Kimore(data,labels,scores)
+    dataloader = DataLoader(dataset,batch_size=batch_size,shuffle = True)
+    return dataloader
