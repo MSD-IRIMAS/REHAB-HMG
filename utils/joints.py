a615bb2ce8739486742bcc9c9a1f92ba2b27e3ec
Nixi <frraroua@gmail.com>
2024-06-04 01:27:56 +0200

    add joints file

---
+++ utils/joints.py
@@ -0,0 +1,24 @@
+JOINT_MAP = {
+    'SpineBase':0,
+    'SpineMid':1,
+    'Head':2,
+    'LeftShoulder':3,
+    'LeftElbow':4,
+    'LeftWrist':5,
+    'LeftHand':6,
+    'RightShoulder':7,
+    'RightElbow':8,
+    'RightWrist':9,
+    'RightHand':10,
+    'RightHip':11,
+    'LeftKnee':12,
+    'LeftAnkle':13,
+    'LeftHip':14,
+    'RightKnee':15,
+    'RightAnkle':16,
+    'Neck':17,
+    
+    
+    
+    }
+
