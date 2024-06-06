639f01905f3700d1d77d8e2a4b26d8b06f84c749
Frr Aroua <ha.ferrar@esi-sba.dz>
2024-06-06 19:06:25 +0200

    classes file

--- utils/classes.py
+++ utils/classes.py
@@ -1,6 +1,8 @@
 
-classes = {"ex1":"arm lift",
-"ex2":"lateral tilit of the trunk",
-"ex3":"trunk rotation",
-"ex4":"pelvis rotation",
-"ex5":"squatting"}
+classes = {
+    "ex1":"arm lift",
+    "ex2":"lateral tilit of the trunk",
+    "ex3":"trunk rotation",
+    "ex4":"pelvis rotation",
+    "ex5":"squatting"
+    }
