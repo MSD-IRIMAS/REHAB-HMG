9e6c3dfe7d35d2290dda5c6e1bc082a5ca620217
Frr Aroua <ha.ferrar@esi-sba.dz>
2024-06-07 20:49:06 +0200

    dtw distance calculation

--- model/regressor.py
+++ model/regressor.py
@@ -1,85 +1,70 @@
 import numpy as np
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torchvision
 import pandas as pd
-from torch.autograd import Variable
-from aeon.regression.distance_based import KNeighborsTimeSeriesRegressor
 import sys
 sys.path.append('/home/hferrar/HMG/dataset')
 sys.path.append('/home/hferrar/HMG/urils')
 from dataset import load_class
 from normalize import normalize_skeletons
 from aeon.distances import dtw_distance
-from fastdtw import fastdtw
-
-
-X_train = np.load('/home/hferrar/HMG/results/run_0/generated_samples/class_0/generated_samples.npy')
-print(X_train.shape)
-y_train = np.load('/home/hferrar/HMG/results/run_0/generated_samples/class_0/true_scores.npy')
-X_train = np.reshape(X_train, (X_train.shape[0], 748, 18*3))
-data,labels,scores = load_class(0,root_dir='/home/hferrar/HMG/data/Kimore/')
-data = normalize_skeletons(data)
-print(data.shape)
-X_test = np.reshape(data, (data.shape[0], data.shape[1], 18*3))
 
 
 
-def calculate_dtw(xtrain,xtest):
+
+def distance_dtw(xtrain, xtest):
+    distances = np.zeros((len(xtrain), len(xtest)))
+
     for i in range(len(xtrain)):
         for j in range(len(xtest)):
             distance = dtw_distance(xtrain[i, :, :], xtest[j, :, :])
-            print("DTW distance for generated sample", i,"and true sample",j,"is: ", distance)
+            distances[i, j] = distance
+
+    return distances
+
+def evaluate_dtw(generated_samples, generated_scores, test_samples, test_scores):
+    distances = distance_dtw(generated_samples, test_samples)
+    
+    total_error = 0
+    score_errors = []
+    closest_indices = []
+    match_count = 0
+    results = []
+    for i in range(len(generated_samples)):
+        min_index = np.argmin(distances[i])
+        closest_indices.append(min_index)
+        closest_score = test_scores[min_index]
+        score_error = abs(closest_score - generated_scores[i])
+        score_errors.append(score_error)
+        total_error += score_error
+        result = f"Generated sample {i} with score {generated_scores[i]:.2f} is closest to test sample {min_index} with of score of {test_scores[min_index]:.2f} ==> error: {score_error:.2f}"
+        results.append(result)
+        if generated_scores[i] == closest_score:
+            match_count += 1
+    mean_score_error = total_error / len(generated_samples)
+    results.append(f"\nMean score error: {mean_score_error:.2f}")
+    results.append(f"\nNumber of matches between generated samples and closest test samples: {match_count}/{len(generated_samples)}")
+    return score_errors, mean_score_error, match_count, results
 
 
-def calculate_dtw_and_save_results(xtrain, xtest, output_file,ytrain):
-    with open(output_file, 'w') as f:
-        for i in range(len(xtrain)):
-            min_distance = float('inf')
-            min_index = None
-            for j in range(len(xtest)):
-                distance = dtw_distance(xtrain[i, :, :], xtest[j, :, :])
-                f.write(f"DTW distance for generated sample {i}  and true sample {j} is: {distance}\n")
-                if distance < min_distance:
-                    min_distance = distance
-                    min_index = j
-            f.write(f"Minimal DTW distance for generated sample {i} with score {ytrain[i]} is: {min_distance} "
-                    f"(achieved with true sample {min_index}) with score {ytrain[min_index]} \n")
+
+def calculate_dtw(class_index):
+    xtrain= np.load(f'/home/hferrar/HMG/results/run_0/generated_samples/class_{class_index}/generated_samples_prior.npy')
+    scores = np.load(f'/home/hferrar/HMG/results/run_0/generated_samples/class_{class_index}/scores.npy').squeeze(0).squeeze(1)
+    xtest = np.load(f'/home/hferrar/HMG/results/run_0/generated_samples/class_{class_index}/true_samples.npy')
+    xtrain = np.reshape(xtrain, (xtrain.shape[0], 748, 18*3))
+    xtest = np.reshape(xtest, (xtest.shape[0], xtest.shape[1], 18*3))
+    print(scores)
+
+    score_errors, mean_score_error, match_count, results = evaluate_dtw(xtrain, scores, xtest, scores)
+    with open(f"/home/hferrar/HMG/results/run_0/prior/dtw_evaluation_results_class_{class_index}.txt", "w") as file:
+        for line in results:
+            file.write(line + "\n")
+    
 
 
-calculate_dtw_and_save_results(X_train, X_test, '/home/hferrar/HMG/results/dtw_results.txt',y_train)
-# print(y_train)
-# print('--------------------------------------------')
-# X_train = np.transpose(X_train, (0, 2, 1))
-# X_test = np.transpose(X_test, (0, 2, 1))
-# reg = KNeighborsTimeSeriesRegressor(distance='dtw')
-# reg.fit(X_train,y_train)
-# pred=reg.predict(X_test)
-# print(pred)
+calculate_dtw(0)
+calculate_dtw(1)
+calculate_dtw(2)
+calculate_dtw(3)
+calculate_dtw(4)
 
 
-
-# calculate_dtw(X_train,X_test)
-
-def write_min_distance_info(file_path, sample_index, min_index, ytrain):
-    with open(file_path, 'a') as f:
-        f.write(f"Minimal DTW distance for generated sample {sample_index} with score {ytrain[sample_index]}  "
-                f"achieved with true sample {min_index} with score {ytrain[min_index]} \n")
-
-def calculate_dtw_and_save_results(xtrain, xtest, output_file, ytrain, min_distance_file):
-    with open(output_file, 'w') as f:
-        for i in range(len(xtrain)):
-            min_distance = float('inf')
-            min_index = None
-            for j in range(len(xtest)):
-                distance = dtw_distance(xtrain[i, :, :], xtest[j, :, :])
-                f.write(f"DTW distance for generated sample {i} and true sample {j} is: {distance}\n")
-                if distance < min_distance:
-                    min_distance = distance
-                    min_index = j
-            f.write(f"Minimal DTW distance for generated sample {i} with score {ytrain[i]} is: {min_distance}\n")
-            write_min_distance_info(min_distance_file, i, min_index, ytrain)
-
-# Example usage
-calculate_dtw_and_save_results(X_train, X_test, '/home/hferrar/HMG/results/dtw_results.txt',y_train, '/home/hferrar/HMG/results/min_distance_file.txt')
