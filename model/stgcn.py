93ef3f0a4a70b163f4445ed6a881a701030d4ae2
Frr Aroua <ha.ferrar@esi-sba.dz>
2024-06-26 18:29:43 +0200

    remove batch norm

--- model/stgcn.py
+++ model/stgcn.py
@@ -12,6 +12,7 @@ sys.path.append('../')
 from utils.graph_layers.graph import Graph
 from utils.graph_layers.temporal_conv import TemporalConvGraph
 from utils.plot import plot_regressor_loss,plot_true_pred_scores
+from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
 
 class st_gcn(nn.Module):
     r"""Applies a spatial temporal graph convolution over an input graph sequence.
@@ -100,7 +101,7 @@ class STGCN(nn.Module):
         self.graph = Graph()
         A = torch.tensor(self.graph.A, dtype=torch.float32, requires_grad=False)
         self.register_buffer('A', A)
-        self.data_bn = nn.BatchNorm1d(3 * A.size(1))
+        # self.data_bn = nn.BatchNorm1d(3 * A.size(1))
         spatial_kernel_size = A.size(0)
         temporal_kernel_size = 9
         kernel_size = (temporal_kernel_size, spatial_kernel_size)
@@ -126,19 +127,25 @@ class STGCN(nn.Module):
         else:
             self.edge_importance = [1] * len(self.stgcn_network)
         # regression layers
-        self.lin1 = nn.Linear(256, 128)
-        self.lin2= nn.Linear(128, 64)
-        self.lin3 = nn.Linear(64, score)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+        self.lin1 = nn.Linear(256, 64)
+        self.lin2= nn.Linear(64, 16)
+        self.lin3 = nn.Linear(16, score)
     
     def forward(self,x):
         x =x.permute(0,3,1,2).unsqueeze(4)
         N, C, T, V, M = x.size()
-        x = x.permute(0, 4, 3, 1, 2).contiguous()
-        x = x.view(N * M, V * C, T)
-        x = self.data_bn(x)
-        x = x.view(N, M, V, C, T)
-        x = x.permute(0, 1, 3, 4, 2).contiguous()
         x = x.view(N * M, C, T, V)
+
+        # x =x.permute(0,3,1,2).unsqueeze(4)
+        # N, C, T, V, M = x.size()
+        # # x = x.permute(0, 4, 3, 1, 2).contiguous()
+        # # x = x.view(N * M, V * C, T)
+        # # x = self.data_bn(x)
+        # # x = x.view(N, M, V, C, T)
+        # # x = x.permute(0, 1, 3, 4, 2).contiguous()
+        # x = x.view(N * M, C, T, V)
         
 
 
@@ -151,9 +158,10 @@ class STGCN(nn.Module):
         x = F.avg_pool2d(x, x.size()[2:])
         x = x.view(N, M, -1, 1, 1).mean(dim=1)
         x = x.view(x.size(0), -1)
-        x = self.lin1(x)
-        x = self.lin2(x)
-        output = self.lin3(x)
+        x = self.relu(self.lin1(x))
+        x = self.relu(self.lin2(x))
+        x = self.lin3(x)
+        output=self.sigmoid(x)
         return output
     
     def loss(self,score,output):
@@ -205,10 +213,38 @@ class STGCN(nn.Module):
             if train_losses[-1] < min_loss:
                 min_loss = train_losses[-1]
                 torch.save(self.state_dict(), os.path.join(self.output_directory, 'best_stgcn.pth'))
+
+
             torch.save(self.state_dict(), os.path.join(self.output_directory, 'last_stgcn.pth'))
         plot_regressor_loss(self.epochs, train_losses, test_losses,self.output_directory)
+    def score_error(self,true_scores,predicted_scores):
+        rmse = mean_squared_error(true_scores,predicted_scores)
+        mae = mean_absolute_error(true_scores,predicted_scores)
+        mape = mean_absolute_percentage_error(true_scores,predicted_scores)
+        print(f'RMSE: {rmse:.4f}')
+        print(f'MAE: {mae:.4f}')
+        print(f'MAPE: {mape:.4f}')
+
+    def square_plot(self,pred_test,valid_y):
+        plt.figure(figsize = (8,8))
+        plt.subplot(2,1,1)
+
+        plt.plot(pred_test,'s', color='red', label='Prediction', linestyle='None', alpha = 0.5, markersize=6)
+        plt.plot(valid_y,'o', color='green',label='True Score', alpha = 0.4, markersize=6)
+        plt.title('Testing Set',fontsize=18)
+        plt.ylim([-0.1,1.1])
+        plt.xlabel('sample Number',fontsize=16)
+        plt.ylabel('Score',fontsize=16)
+        plt.legend(loc=3, prop={'size':14}) # loc:position
+        plt.tight_layout()
+        title='score comparaison'
+        plt.savefig(os.path.join(self.output_directory, title+'.pdf'))
+        plt.show()
+
     
     def predict_scores(self, test_loader,device):
+        device = self.device
+        self.to(device)
         num_samples = test_loader.dataset.__len__()
         true_scores = []
         predicted_scores = []
@@ -217,7 +253,7 @@ class STGCN(nn.Module):
         with torch.no_grad():
             for i in range(num_samples):
                 input_tensor = test_loader.dataset[i]
-                data = input_tensor[0].unsqueeze(0)
+                data = input_tensor[0].unsqueeze(0).to(device)
                 true_score = input_tensor[2].item()
                 prediction = self(data)
                 predicted_score = prediction.item()
@@ -226,9 +262,33 @@ class STGCN(nn.Module):
             
                 print(f'Sample: {i}/{num_samples}, True Score: {true_score:.4f}, Predicted Score: {predicted_score:.4f}')
             plot_true_pred_scores(predicted_scores,true_scores,self.output_directory)
-       
+            self.square_plot(predicted_scores,true_scores)
+            self.score_error(true_scores,predicted_scores)
 
+    
+
+
+
+
+  #####################" REFACTOR THIS FUNCTION ##########################"  
+
+    def test_predictions(self,device):
+        self.device=device
+        self.to(device)
+        self.load_state_dict(torch.load(self.output_directory + 'best_stgcn.pth', map_location=device))
+        self.eval()
+        
+        data = np.load('./results/run_0/cross_validation/class_0/fold_1/generated_samples/generated_samples_prior.npy')
+        scores=np.load('./results/run_0/cross_validation/class_0/fold_1/generated_samples/scores.npy')
+        data = torch.squeeze(torch.tensor(data).float(),1)
+        scores = torch.tensor(scores).float()
+        data=data.to(device)
   
+        prediction= self(data)
+        
+        print(prediction)
+        print('-------------------------------------')
+        print(scores / 100)
 
-
+       
 
