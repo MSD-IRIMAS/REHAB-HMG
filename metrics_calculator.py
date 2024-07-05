b4fe40161436e41049bd0dd14e40981d8fd403ca
Frr Aroua <ha.ferrar@esi-sba.dz>
2024-07-05 18:35:51 +0200

    Metrics calculator : fid,mms,coverage

---
+++ metrics_calculator.py
@@ -0,0 +1,295 @@
+def warn(*args, **kwargs):
+    pass
+import os
+import torch
+import warnings
+import argparse
+import numpy as np
+import pandas as pd
+warnings.warn = warn
+from model.cvae import CVAE
+from model.stgcn import STGCN
+from utils.metrics import FID,Coverage,MMS,FeatureExtractor
+from utils.visualize import create_directory
+from dataset.dataset import Kimore, load_class
+from torch.utils.data import DataLoader,Subset
+from utils.utils import get_weights_loss,get_dirs
+from sklearn.model_selection import train_test_split
+from utils.normalize import normalize_skeletons
+
+def get_args():
+    parser = argparse.ArgumentParser(
+    description="training STGCN to predict generated data score")
+
+    parser.add_argument(
+        '--regression_models',
+        type=str,
+        choices=['STGCN','REG'],
+        default='STGCN',
+    )
+    parser.add_argument(
+        '--generative_model',
+        type=str,
+        default='CVAE',
+    )
+
+    parser.add_argument(
+        '--dataset',
+        help="Which dataset to use.",
+        type=str,
+        default='Kimore'
+    )
+
+    parser.add_argument(
+        '--output-directory',
+        type=str,
+        default='results/'
+    )
+
+    parser.add_argument(
+        '--runs',
+        help="Number of experiments to do.",
+        type=int,
+        default=5
+    )
+    parser.add_argument(
+        '--device',
+        help="Device to run the training on.",
+        type=str,
+        choices=['cpu', 'cuda', 'mps'],
+        default='cuda' if torch.cuda.is_available() else ('mps' if torch.backends.mps.is_available() else 'cpu')
+    )
+
+    parser.add_argument(
+        '--data_split',
+        help="choose wether split the data or use it all",
+        type=str,
+        choices=['all', 'split'],
+        default='split'
+    )
+    parser.add_argument(
+        '--class_index',
+        help="which class to generate",
+        type=int,
+        default=0
+    )
+    parser.add_argument(
+        '--on',
+        type=str,
+        choices = ['real','generated'],
+        default='generated',
+    )
+    parser.add_argument(
+        '--wrec',
+        help="Weight for the reconstruction loss.",
+        type=float,
+        default=0.99
+    )
+
+    parser.add_argument(
+        '--wkl',
+        help="Weight for the kl loss.",
+        type=float,
+        default=1e-3
+    )
+
+    args = parser.parse_args()
+
+    return args
+    
+if __name__ == "__main__":
+
+    args = get_args()
+
+    output_directory_results = args.output_directory + 'regression_models/'
+    create_directory(output_directory_results)
+   
+    dataset_dir = 'data/' + args.dataset + '/'
+    data,labels,scores = load_class(args.class_index,root_dir=dataset_dir)
+   
+    xtrain,xtest,ytrain,ytest,strain,stest= train_test_split(data,labels,scores,test_size=0.2,random_state=42)
+    xtrain,min_X, max_X,min_Y,max_Y, min_Z,max_Z= normalize_skeletons(xtrain)
+    train_set = Kimore(xtrain,ytrain,strain)
+    train_loader = DataLoader(train_set,batch_size=16,shuffle =True)
+    xtest,_,_,_,_,_,_= normalize_skeletons(xtest,min_X, max_X,min_Y,max_Y, min_Z,max_Z)
+    test_set = Kimore(xtest,ytest,stest)
+    test_loader = DataLoader(test_set,batch_size=16,shuffle=False)
+
+    output_directory_run = args.output_directory + 'run_' + str(args.runs) + '/'
+    create_directory(output_directory_run)
+    output_directory_skeletons_class = output_directory_run + 'class_' + str(args.class_index) + '/'
+    create_directory(output_directory_skeletons_class)
+
+
+
+
+    if args.regression_models == 'STGCN':
+        stgcn_directory = output_directory_results + 'STGCN/'
+        stgcn_run_dir_class = stgcn_directory + 'run_' + str(args.runs) +'/class_'+str(args.class_index)
+        stgcn_run_dir = stgcn_run_dir_class + '/best_stgcn.pth'
+        feature_extractor_stgcn = FeatureExtractor(model_name=args.regression_models, model_path=stgcn_run_dir, device=args.device)
+        fid_calculator_stgcn = FID(feature_extractor_stgcn)
+        coverage_calculator_stgcn = Coverage(feature_extractor_stgcn)
+        mms_calculator_stgcn = MMS(feature_extractor_stgcn)
+        print(feature_extractor_stgcn)
+        if args.on == 'real':
+            if os.path.exists(stgcn_directory + 'metrics_results.csv'):
+                df = pd.read_csv(stgcn_directory + 'metrics_results.csv')
+            else:
+                df = pd.DataFrame(columns=['class','FID','COV','MMS'])
+            #FID metric
+            fid_score = fid_calculator_stgcn.calculate_fid(train_loader, test_loader)
+            fid_mean = np.mean(fid_score)
+            print('----------------------------------------------fid done')
+
+            #COV metric
+            cov_score = coverage_calculator_stgcn.calculate(test_loader, train_loader)
+            cov_mean = np.mean(cov_score)
+            print('----------------------------------------------cov done')
+
+
+            #MMS metric
+            mms_score = mms_calculator_stgcn.calculate_mms(test_loader, train_loader)
+            mms_mean = np.mean(mms_score)
+          
+            if args.class_index in df['class'].values:
+                df.loc[df['class'] == args.class_index, 'FID'] = fid_mean
+                df.loc[df['class'] == args.class_index, 'COV'] = cov_mean
+                df.loc[df['class'] == args.class_index, 'MMS'] = mms_mean
+            else:
+                new_row = pd.DataFrame([{'class': args.class_index, 'FID': fid_mean, 'COV': cov_mean, 'MMS': mms_mean}])
+                df = pd.concat([df, new_row], ignore_index=True)
+                      
+            df.to_csv(stgcn_directory + 'metrics_results.csv', index=False)
+
+            
+        elif args.on == 'generated':
+            weights_loss = {
+                'wrec' : args.wrec,
+                'wkl' : args.wkl,}
+            if os.path.exists(stgcn_directory + 'metrics_results_generated.csv'):
+                df = pd.read_csv(stgcn_directory + 'metrics_results_generated.csv')
+            else:
+                df = pd.DataFrame(columns=['class','FID','COV','MMS'])
+            
+            #generate samples 
+            generator = CVAE(output_directory=output_directory_skeletons_class,
+                    device=args.device,
+                    w_rec=weights_loss['wrec'],
+                    w_kl=weights_loss['wkl'])
+            generated_samples,gen_scores = generator.generate_samples_from_prior(device = args.device,class_index=args.class_index,gif_directory=stgcn_run_dir_class,dataloader=test_loader)
+            gen_set = Kimore(generated_samples,labels,gen_scores)
+            gen_loader = DataLoader(gen_set,batch_size=16,shuffle =True)
+            
+            #FID metric
+            fid_score = fid_calculator_stgcn.calculate_fid(gen_loader, test_loader)
+            fid_mean = np.mean(fid_score)
+            print('----------------------------------------------fid done')
+
+            #COV metric
+            cov_score = coverage_calculator_stgcn.calculate(xgenerated_loader=gen_loader, xreal_loader=test_loader)
+            cov_mean = np.mean(cov_score)
+            print('----------------------------------------------cov done')
+
+
+            #MMS metric
+            mms_score = mms_calculator_stgcn.calculate_mms(generated_data=gen_loader, real_data=test_loader)
+            mms_mean = np.mean(mms_score)
+          
+            if args.class_index in df['class'].values:
+                df.loc[df['class'] == args.class_index, 'FID'] = fid_mean
+                df.loc[df['class'] == args.class_index, 'COV'] = cov_mean
+                df.loc[df['class'] == args.class_index, 'MMS'] = mms_mean
+            else:
+                new_row = pd.DataFrame([{'class': args.class_index, 'FID': fid_mean, 'COV': cov_mean, 'MMS': mms_mean}])
+                df = pd.concat([df, new_row], ignore_index=True)
+            df.to_csv(stgcn_directory + 'metrics_results_generated.csv', index=False)
+
+
+
+
+    elif args.regression_models == 'REG':
+        regressor_directory = output_directory_results + 'REG/'
+        reg_run_dir_class = regressor_directory + 'run_' + str(args.runs) +'/class_'+str(args.class_index)
+        reg_run_dir = reg_run_dir_class + '/best_regressor.pth'
+        
+        feature_extractor_reg = FeatureExtractor(model_name=args.regression_models, model_path=reg_run_dir, device=args.device)
+
+        fid_calculator = FID(feature_extractor_reg)
+        coverage_calculator = Coverage(feature_extractor_reg)
+        mms_calculator= MMS(feature_extractor_reg)
+
+
+        if args.on == 'real':
+            if os.path.exists(regressor_directory + 'metrics_results.csv'):
+                df = pd.read_csv(regressor_directory + 'metrics_results.csv')
+            else:
+                df = pd.DataFrame(columns=['class','FID','COV','MMS'])
+            #FID metric
+            fid_score = fid_calculator.calculate_fid(train_loader, test_loader)
+            fid_mean = np.mean(fid_score)
+            print('----------------------------------------------fid done')
+
+            #COV metric
+            cov_score = coverage_calculator.calculate(train_loader, test_loader)
+            cov_mean = np.mean(cov_score)
+            print('----------------------------------------------cov done')
+
+            #MMS metric
+            mms_score = mms_calculator.calculate_mms(train_loader, test_loader)
+            mms_mean = np.mean(mms_score)
+          
+            if args.class_index in df['class'].values:
+                df.loc[df['class'] == args.class_index, 'FID'] = fid_mean
+                df.loc[df['class'] == args.class_index, 'COV'] = cov_mean
+                df.loc[df['class'] == args.class_index, 'MMS'] = mms_mean
+            else:
+                new_row = pd.DataFrame([{'class': args.class_index, 'FID': fid_mean, 'COV': cov_mean, 'MMS': mms_mean}])
+                df = pd.concat([df, new_row], ignore_index=True)
+                      
+            df.to_csv(regressor_directory + 'metrics_results.csv', index=False)
+            
+        elif args.on == 'generated':
+            weights_loss = {
+                'wrec' : args.wrec,
+                'wkl' : args.wkl,}
+            if os.path.exists(regressor_directory + 'metrics_results_generated.csv'):
+                df = pd.read_csv(regressor_directory + 'metrics_results_generated.csv')
+            else:
+                df = pd.DataFrame(columns=['class','FID','COV','MMS'])
+            
+            #generate samples 
+            generator = CVAE(output_directory=output_directory_skeletons_class,
+                    device=args.device,
+                    w_rec=weights_loss['wrec'],
+                    w_kl=weights_loss['wkl'])
+            generated_samples,gen_scores = generator.generate_samples_from_prior(device = args.device,class_index=args.class_index,gif_directory=reg_run_dir_class,dataloader=test_loader)
+            gen_set = Kimore(generated_samples,labels,gen_scores)
+            gen_loader = DataLoader(gen_set,batch_size=16,shuffle =True)
+            
+            #FID metric
+            fid_score = fid_calculator.calculate_fid(gen_loader, test_loader)
+            fid_mean = np.mean(fid_score)
+            print('----------------------------------------------fid done')
+            #COV metric
+            cov_score = coverage_calculator.calculate(gen_loader, test_loader)
+            cov_mean = np.mean(cov_score)
+            print('----------------------------------------------cov done')
+            #MMS metric
+            mms_score = mms_calculator.calculate_mms(gen_loader, test_loader)
+            mms_mean = np.mean(mms_score)
+
+            if args.class_index in df['class'].values:
+                df.loc[df['class'] == args.class_index, 'FID'] = fid_mean
+                df.loc[df['class'] == args.class_index, 'COV'] = cov_mean
+                df.loc[df['class'] == args.class_index, 'MMS'] = mms_mean
+            else:
+                new_row = pd.DataFrame([{'class': args.class_index, 'FID': fid_mean, 'COV': cov_mean, 'MMS': mms_mean}])
+                df = pd.concat([df, new_row], ignore_index=True)
+            df.to_csv(regressor_directory + 'metrics_results_generated.csv', index=False)
+
+
+           
+        
+
+
--- utils/metrics.py
+++ utils/metrics.py
@@ -8,38 +8,55 @@ import torch.nn as nn
 sys.path.append('../')
 from model.stgcn import STGCN
 from scipy.linalg import sqrtm 
+from model.regressor import REG
 from numpy.lib import scimath as sc
 from sklearn.metrics import pairwise_distances
 from sklearn.neighbors import NearestNeighbors
 from dataset.dataset import load_class, Kimore
 from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader, TensorDataset,Subset
-from utils.normalize import normalize_skeletons, normalize_test_set
-class FID:
-    def __init__(self, model_path, device, data_loader):
+
+
+class FeatureExtractor:
+    def __init__(self, model_name, model_path, device):
         self.device = torch.device(device)
-        self.model = STGCN(device=device, output_directory='./')
-        self.model.load_state_dict(torch.load(model_path, map_location=device))
+        self.model_name = model_name
+        
+        if model_name == 'STGCN':
+            self.model = STGCN(device=device, output_directory='./')
+            self.model.load_state_dict(torch.load(model_path, map_location=device))
+            # self.features_extractor = nn.Sequential(*list(self.model.children())[:-3])
+        elif model_name == 'REG':
+            self.model = REG(device=device, output_directory='./')
+            self.model.load_state_dict(torch.load(model_path, map_location=device))
+            
+        else:
+            raise ValueError(f"Unsupported model name: {model_name}")
+        
+        
         self.model.to(self.device)
         self.model.eval()
-        self.data_loader = data_loader
-        self.features_extractor = nn.Sequential(*list(self.model.children())[:-3])  
+
     def extract_features(self, data_loader):
         features = []
-        self.features_extractor.eval()
+        # self.features_extractor.eval()
         with torch.no_grad():
-            for data, _, s in data_loader:
+            for  batch_idx, (data,_, score) in enumerate(data_loader):
                 data = data.to(self.device)
-                output = self.model(data, feature_extractor=True)
-                print(output)
-                features.append(output.cpu().detach().numpy()) 
-        features = np.concatenate(features, axis=0)  
+                if self.model_name == 'STGCN':
+                    output = self.model(data, feature_extractor=True)
+                elif self.model_name =='REG':
+                    # 'REG'
+                    self.model.eval()
+                    output = self.model(data,extract_feature=True)
+                    
+                features.append(output.cpu().detach().numpy())
+        features = np.concatenate(features, axis=0)
         return features
 
-    def calculate_fid(self, data_loader_x, data_loader_y):
-        features_x = self.extract_features(data_loader_x)
-        features_y = self.extract_features(data_loader_y)
 
+
+def calculate_fid( features_x, features_y):
+        
         mean_x = np.mean(features_x, axis=0)
         mean_y = np.mean(features_y, axis=0)
 
@@ -55,54 +72,34 @@ class FID:
         trace_sqrt = np.trace(cov_sqrt_product)
         fid = mean_squared_norm + np.trace(cov_x) + np.trace(cov_y) - 2 * trace_sqrt
         return fid
+class FID:
+    def __init__(self, feature_extractor):
+        self.feature_extractor = feature_extractor
 
+    def calculate_fid(self, data_loader_x, data_loader_y):
+        
+        features_x = self.feature_extractor.extract_features(data_loader_x)
+        features_y = self.feature_extractor.extract_features(data_loader_y)
+        print(features_x.shape)
+        mean_x = np.mean(features_x, axis=0)
+        mean_y = np.mean(features_y, axis=0)
 
-class Diversity:
-    def __init__(self, model_path, device, data_loader):
-        self.device = torch.device(device)
-        self.model = STGCN(device=device, output_directory='./')
-        self.model.load_state_dict(torch.load(model_path, map_location=device))
-        self.model.to(self.device)
-        self.model.eval()
-        self.data_loader = data_loader
-        self.features_extractor = nn.Sequential(*list(self.model.children())[:-3])  
-    def extract_features(self, data_loader):
-        features = []
-        self.features_extractor.eval()
-        with torch.no_grad():
-            for data, _, s in data_loader:
-                data = data.to(self.device)
-                output = self.model(data, feature_extractor=True)
-                print(output)
-                features.append(output.cpu().detach().numpy()) 
-        features = np.concatenate(features, axis=0)  
-        return features
+        cov_x = np.cov(features_x, rowvar=False)
+        cov_y = np.cov(features_y, rowvar=False)
 
-    def calculate_diversity(self,data_loader_x,x,Sd):
-        pass
+        mean_diff = mean_x - mean_y
+        mean_squared_norm = np.dot(mean_diff, mean_diff)
 
-
+        cov_sqrt_product = sqrtm(np.dot(cov_x, cov_y))
+        if np.iscomplexobj(cov_sqrt_product):
+            cov_sqrt_product = cov_sqrt_product.real
+        trace_sqrt = np.trace(cov_sqrt_product)
+        fid = mean_squared_norm + np.trace(cov_x) + np.trace(cov_y) - 2 * trace_sqrt
+        return fid
 class Coverage:
-    def __init__(self, model_path, device, data_loader, n_neighbors=5):
-        self.device = torch.device(device)
-        self.model = STGCN(device=device, output_directory='./')
-        self.model.load_state_dict(torch.load(model_path, map_location=device))
-        self.model.to(self.device)
-        self.model.eval()
-        self.data_loader = data_loader
-        self.features_extractor = nn.Sequential(*list(self.model.children())[:-3])
+    def __init__(self, feature_extractor, n_neighbors=5):
+        self.feature_extractor = feature_extractor
         self.n_neighbors = n_neighbors
-
-    def extract_features(self, data_loader):
-        features = []
-        self.features_extractor.eval()
-        with torch.no_grad():
-            for data, _, _ in data_loader:
-                data = data.to(self.device)
-                output = self.features_extractor(data)
-                features.append(output.cpu().detach().numpy())
-        features = np.concatenate(features, axis=0)
-        return features
 
     def get_distances_k_neighbors(self, x: np.ndarray, k: int):
         nn_model = NearestNeighbors(n_neighbors=k)
@@ -111,8 +108,8 @@ class Coverage:
         return distances_neighbors[:, k - 1]
 
     def calculate(self, xgenerated_loader=None, xreal_loader=None):
-        real_latent = self.extract_features(xreal_loader)
-        gen_latent = self.extract_features(xgenerated_loader)
+        real_latent = self.feature_extractor.extract_features(xreal_loader)
+        gen_latent = self.feature_extractor.extract_features(xgenerated_loader)
 
         real_gen_distance_matrix = pairwise_distances(X=real_latent, Y=gen_latent)
         real_distances_k_neighbors = self.get_distances_k_neighbors(x=real_latent, k=self.n_neighbors + 1)
@@ -125,35 +122,140 @@ class Coverage:
 
 
 class MMS:
-    def __init__(self, model_path, device, data_loader, n_neighbors=5):
-        self.device = torch.device(device)
-        self.model = STGCN(device=device, output_directory='./')
-        self.model.load_state_dict(torch.load(model_path, map_location=device))
-        self.model.to(self.device)
-        self.model.eval()
-        self.data_loader = data_loader
-        self.features_extractor = nn.Sequential(*list(self.model.children())[:-3])
+    def __init__(self, feature_extractor, n_neighbors=5):
+        self.feature_extractor = feature_extractor
         self.n_neighbors = n_neighbors
 
-    def extract_features(self, data_loader):
-        features = []
-        self.features_extractor.eval()
-        with torch.no_grad():
-            for data, _, _ in data_loader:
-                data = data.to(self.device)
-                output = self.features_extractor(data)
-                features.append(output.cpu().detach().numpy())
-        features = np.concatenate(features, axis=0)
-        return features 
-    
-    def calculate_mms(self,generated_data,real_data):
-        real_latent = self.extract_features(xreal_loader)
-        gen_latent = self.extract_features(xgenerated_loader)
-        nn = NearestNeighbors(n_neighbors=k)
+    def calculate_mms(self, generated_data, real_data):
+        real_latent = self.feature_extractor.extract_features(real_data)
+        gen_latent = self.feature_extractor.extract_features(generated_data)
+        
+        nn = NearestNeighbors(n_neighbors=self.n_neighbors)
         nn.fit(real_latent)
         distances, _ = nn.kneighbors(X=gen_latent, return_distance=True)
+        
         if distances.shape[-1] > 1:
-            print(distances.shape)
             distances = distances[:, 1]
 
         return np.mean(distances)
+
+
+
+
+    
+# class FID:
+#     def __init__(self, model_path, device, data_loader):
+#         self.device = torch.device(device)
+#         self.model = STGCN(device=device, output_directory='./')
+#         self.model.load_state_dict(torch.load(model_path, map_location=device))
+#         self.model.to(self.device)
+#         self.model.eval()
+#         self.data_loader = data_loader
+#         # self.features_extractor = nn.Sequential(*list(self.model.children())[:-3])  
+#     def extract_features(self, data_loader):
+#         features = []
+#         self.features_extractor.eval()
+#         with torch.no_grad():
+#             for data, _, s in data_loader:
+#                 data = data.to(self.device)
+#                 output = self.model(data, feature_extractor=True)
+               
+                
+#                 features.append(output.cpu().detach().numpy()) 
+#         features = np.concatenate(features, axis=0)  
+#         return features
+
+#     def calculate_fid(self, data_loader_x, data_loader_y):
+#         features_x = self.extract_features(data_loader_x)
+#         features_y = self.extract_features(data_loader_y)
+
+#         mean_x = np.mean(features_x, axis=0)
+#         mean_y = np.mean(features_y, axis=0)
+
+#         cov_x = np.cov(features_x, rowvar=False)
+#         cov_y = np.cov(features_y, rowvar=False)
+
+#         mean_diff = mean_x - mean_y
+#         mean_squared_norm = np.dot(mean_diff, mean_diff)
+
+#         cov_sqrt_product = sqrtm(np.dot(cov_x, cov_y))
+#         if np.iscomplexobj(cov_sqrt_product):
+#             cov_sqrt_product = cov_sqrt_product.real
+#         trace_sqrt = np.trace(cov_sqrt_product)
+#         fid = mean_squared_norm + np.trace(cov_x) + np.trace(cov_y) - 2 * trace_sqrt
+#         return fid
+# class Coverage:
+#     def __init__(self, model_path, device, data_loader, n_neighbors=5):
+#         self.device = torch.device(device)
+#         self.model = STGCN(device=device, output_directory='./')
+#         self.model.load_state_dict(torch.load(model_path, map_location=device))
+#         self.model.to(self.device)
+#         self.model.eval()
+#         self.data_loader = data_loader
+#         # self.features_extractor = nn.Sequential(*list(self.model.children())[:-3])
+#         self.n_neighbors = n_neighbors
+
+#     def extract_features(self, data_loader):
+#         features = []
+#         self.features_extractor.eval()
+#         with torch.no_grad():
+#             for data, _, _ in data_loader:
+#                 data = data.to(self.device)
+#                 output = self.model(data, feature_extractor=True)
+#                 features.append(output.cpu().detach().numpy())
+#         features = np.concatenate(features, axis=0)
+#         return features
+
+#     def get_distances_k_neighbors(self, x: np.ndarray, k: int):
+#         nn_model = NearestNeighbors(n_neighbors=k)
+#         nn_model.fit(X=x)
+#         distances_neighbors, _ = nn_model.kneighbors(X=x)
+#         return distances_neighbors[:, k - 1]
+
+#     def calculate(self, xgenerated_loader=None, xreal_loader=None):
+#         real_latent = self.extract_features(xreal_loader)
+#         gen_latent = self.extract_features(xgenerated_loader)
+
+#         real_gen_distance_matrix = pairwise_distances(X=real_latent, Y=gen_latent)
+#         real_distances_k_neighbors = self.get_distances_k_neighbors(x=real_latent, k=self.n_neighbors + 1)
+
+#         distances_nearest_neighbor_real_to_gen = np.min(real_gen_distance_matrix, axis=1)
+#         exists_inside_neighborhood = distances_nearest_neighbor_real_to_gen < real_distances_k_neighbors
+#         coverage = np.mean(exists_inside_neighborhood)
+
+#         return coverage
+
+
+# class MMS:
+#     def __init__(self, model_path, device, data_loader, n_neighbors=5):
+#         self.device = torch.device(device)
+#         self.model = STGCN(device=device, output_directory='./')
+#         self.model.load_state_dict(torch.load(model_path, map_location=device))
+#         self.model.to(self.device)
+#         self.model.eval()
+#         self.data_loader = data_loader
+#         # self.features_extractor = nn.Sequential(*list(self.model.children())[:-3])
+#         self.n_neighbors = n_neighbors
+
+#     def extract_features(self, data_loader):
+#         features = []
+#         self.features_extractor.eval()
+#         with torch.no_grad():
+#             for data, _, _ in data_loader:
+#                 data = data.to(self.device)
+#                 output = self.model(data, feature_extractor=True)
+#                 features.append(output.cpu().detach().numpy())
+#         features = np.concatenate(features, axis=0)
+#         return features 
+    
+#     def calculate_mms(self,generated_data,real_data):
+#         real_latent = self.extract_features(real_data)
+#         gen_latent = self.extract_features(generated_data)
+#         nn = NearestNeighbors(n_neighbors=self.n_neighbors)
+#         nn.fit(real_latent)
+#         distances, _ = nn.kneighbors(X=gen_latent, return_distance=True)
+#         if distances.shape[-1] > 1:
+           
+#             distances = distances[:, 1]
+
+#         return np.mean(distances)
