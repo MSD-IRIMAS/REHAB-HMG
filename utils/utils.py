c05924200fd6b06f13573983053e466e5b6ee709
iivvy <frraroua@gmail.com>
2024-07-23 19:23:06 +0200

    utils file

--- utils/utils.py
+++ utils/utils.py
@@ -1,22 +1,17 @@
 import os
 import numpy as np
 def get_dirs(dir):
-
     for _, dirs, _ in os.walk(dir):
         return dirs
 
 
 
 def get_weights_loss(dir):
-
     weights = dir.split('_')
     weights_dict = {}
-
     for i in range(len(weights)):
-
         if weights[i][0] == 'W':
-            weights_dict[weights[i]] = float(weights[i+1])
-            
+            weights_dict[weights[i]] = float(weights[i+1])  
     return weights_dict
 
 
