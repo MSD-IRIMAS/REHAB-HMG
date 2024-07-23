279427c72856555fdfacd63a6f155759d9c29ef1
iivvy <frraroua@gmail.com>
2024-07-23 19:17:49 +0200

    KNN on latent space

--- utils/KNN.py
+++ utils/KNN.py
@@ -12,7 +12,8 @@ from model.stgcn import STGCN
 import argparse
 from scipy.linalg import sqrtm, eig
 from model.regressor import REG
-from model.cvae import CVAE
+from model.svae import SVAE
+from model.AS_CVAE import ASCVAE
 from numpy.lib import scimath as sc
 from sklearn.metrics import pairwise_distances
 from sklearn.neighbors import NearestNeighbors
@@ -46,7 +47,7 @@ def get_args():
     parser.add_argument(
         '--generative_model',
         type=str,
-        default='CVAE',
+        default='ASCVAE',
     )
 
     parser.add_argument(
@@ -66,7 +67,7 @@ def get_args():
         '--runs',
         help="Number of experiments to do.",
         type=int,
-        default=4
+        default=0
     )
     parser.add_argument(
         '--device',
@@ -115,7 +116,7 @@ if __name__ == "__main__":
     output_directory_gen_models = output_directory_results + 'Generative_models/'
     create_directory(output_directory_gen_models)
 
-    output_directory_dataset = output_directory_gen_models + args.dataset + '/'
+    output_directory_dataset = output_directory_gen_models + 'Score+Action_conditioned' + '/'
     create_directory(output_directory_dataset)
 
     output_directory_generator = output_directory_dataset + args.generative_model + '/'
@@ -192,7 +193,7 @@ if __name__ == "__main__":
                 'wrec' : args.wrec,
                 'wkl' : args.wkl,}
             #generate samples 
-        generator = CVAE(output_directory=output_directory_skeletons_class,
+        generator = ASCVAE(output_directory=output_directory_run,
                     device=args.device,
                     w_rec=weights_loss['wrec'],
                     w_kl=weights_loss['wkl'])
@@ -202,7 +203,7 @@ if __name__ == "__main__":
                     stgcn_run_dir_class = stgcn_directory + 'run_' + str(args.runs) +'/class_'+str(args.class_index)
                     stgcn_run_dir = stgcn_run_dir_class + '/best_stgcn.pth'
 
-                    generated_samples,gen_scores = generator.generate_samples_from_prior(device = args.device,class_index=args.class_index,gif_directory=stgcn_run_dir_class,dataloader=test_loader)
+                    generated_samples,gen_scores = generator.generate_samples_from_prior(device = args.device,gif_directory=stgcn_run_dir_class,dataloader=test_loader,class_index=args.class_index)
                     gen_set = Kimore(generated_samples,labels,gen_scores)
                     gen_loader = DataLoader(gen_set,batch_size=10,shuffle =True)
                     feature_extractor_stgcn = FeatureExtractor(model_name=args.regression_models, model_path=stgcn_run_dir, device=args.device)
@@ -212,6 +213,9 @@ if __name__ == "__main__":
                     strain=strain.squeeze(1)
                     regressor.fit(train_data, strain)
                     y_pred = regressor.predict(test_features)
+                    gen_scores=np.unique(gen_scores)
+
+                    print(len(gen_scores),len(y_pred))
                     for i in range(len(y_pred)): 
                         print(f'true score {gen_scores[i]:.2f}, predicted score {y_pred[i]:.2f}')
                     print(rmse(y_pred,gen_scores))
@@ -222,10 +226,9 @@ if __name__ == "__main__":
                     regressor_directory = output_directory_results + 'REG/'
                     reg_run_dir_class = regressor_directory + 'run_' + str(args.runs) +'/class_'+str(args.class_index)
                     reg_run_dir = reg_run_dir_class + '/best_regressor.pth'
-                    generated_samples,gen_scores = generator.generate_samples_from_prior(device = args.device,class_index=args.class_index,gif_directory=reg_run_dir_class,dataloader=test_loader)  
+                    generated_samples,gen_scores = generator.generate_samples_from_prior(device = args.device,gif_directory=reg_run_dir_class,dataloader=test_loader,class_index=args.class_index)
                     gen_set = Kimore(generated_samples,labels,gen_scores)
-                    gen_loader = DataLoader(gen_set,batch_size=10,shuffle =True)
-                                
+                    gen_loader = DataLoader(gen_set,batch_size=16,shuffle =True)
                     feature_extractor_reg = FeatureExtractor(model_name=args.regression_models, model_path=reg_run_dir, device=args.device)
                     train_data = feature_extractor_reg.extract_features(train_loader)
                     test_features= feature_extractor_reg.extract_features(gen_loader)
@@ -233,6 +236,7 @@ if __name__ == "__main__":
                     strain=strain.squeeze(1)
                     regressor.fit(train_data, strain)
                     y_pred = regressor.predict(test_features)
+                    gen_scores=np.unique(gen_scores)
                 
                     for i in range(len(y_pred)): 
                         
@@ -272,164 +276,3 @@ if __name__ == "__main__":
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def calculate_dtw_and_save_results(xtrain, xtest, output_file, ytrain, min_distance_file, class_index):
-#     xtrain = np.reshape(xtrain, (xtrain.shape[0], 748, 18 * 3))
-#     xtest = np.reshape(xtest, (xtest.shape[0], xtest.shape[1], 18 * 3))
-
-#     errors = []
-#     total_error =0
-
-#     with open(output_file, 'w') as f:
-#         for i in range(len(xtrain)):
-#             min_distance = float('inf')
-#             min_index = None
-#             for j in range(len(xtest)):
-#                 distance = dtw_distance(xtrain[i, :, :], xtest[j, :, :])
-#                 f.write(f"DTW distance for generated sample {i} and true sample {j} is: {distance} \\\ \n")
-#                 if distance < min_distance:
-#                     min_distance = distance
-#                     min_index = j
-#             error = abs(ytrain[i] - ytrain[min_index])
-#             errors.append(error)
-#             total_error += error
-           
-#             f.write(f"Minimal DTW distance for generated sample {i} with score {ytrain[i]:.2f} is {min_distance} "
-#                     f"of the true sample {min_index} with score {ytrain[min_index]:.2f} ==> error {error}\\\ \n")
-#             write_min_distance_info(min_distance_file, i, min_index, ytrain)
-
-#     # Calculate mean error and standard deviation
-#     mean_error = np.mean(errors)
-#     print('mean',mean_error)
-#     print('total/len',total_error/len(xtrain))
-#     std_error = np.std(errors)
-#     print('std_error',std_error)
-#     # Save mean error and standard deviation
-#     with open(output_file, 'a') as f:
-#         f.write(f"\nMean error: {mean_error}\n")
-#         f.write(f"Standard deviation of error: {std_error}\n")
-
-# def write_min_distance_info(file_path, sample_index, min_index, ytrain):
-#     with open(file_path, 'a') as f:
-#         f.write(f"Minimal DTW distance for generated sample {sample_index} with score {ytrain[sample_index]:.2f}  "
-#                 f"achieved with true sample {min_index} with score {ytrain[min_index]:.2f} \n")
-
-            
-            
-
-
-
-
-
-
-# def calculate_dtw(xtrain,xtest):
-#     xtrain = np.reshape(xtrain, (xtrain.shape[0], 748, 18*3))
-#     xtest = np.reshape(xtest, (xtest.shape[0], xtest.shape[1], 18*3))
-#     for i in range(len(xtrain)):
-#         for j in range(len(xtest)):
-#             distance = dtw_distance(xtrain[i, :, :], xtest[j, :, :])
-#             print("DTW distance for generated sample", i,"and true sample",j,"is: ", distance)
-
-
-# def calculate_dtw_and_save_results(xtrain, xtest, output_file,ytrain,class_index):
-#     with open(output_file, 'w') as f:
-#         for i in range(len(xtrain)):
-#             min_distance = float('inf')
-#             min_index = None
-#             for j in range(7):
-#                 distance = dtw_distance(xtrain[i, :, :], xtest[j, :, :])
-#                 f.write(f"DTW distance for generated sample {i}  and true sample {j} is: {distance}\n")
-#                 if distance < min_distance:
-#                     min_distance = distance
-#                     min_index = j
-#             f.write(f"Minimal DTW distance for generated sample {i} with score {ytrain[i]:.2f} is: {min_distance} "
-#                     f"(achieved with true sample {min_index}) with score {ytrain[min_index]:.2f} \n")
-
-
-# # calculate_dtw_and_save_results(xtrain, data, f'../results/run_0/class_{class_index}/dtw_evaluation_results_prior.txt',scores,class_index)
-
-
-# def write_min_distance_info(file_path, sample_index, min_index, ytrain):
-#     with open(file_path, 'a') as f:
-#         f.write(f"Minimal DTW distance for generated sample {sample_index} with score {ytrain[sample_index]:.2f}  "
-#                 f"achieved with true sample {min_index} with score {ytrain[min_index]:.2f} \n")
-
-# def calculate_dtw_and_save_results(xtrain, xtest, output_file, ytrain, min_distance_file,class_index):
-#     xtrain = np.reshape(xtrain, (xtrain.shape[0], 748, 18*3))
-#     xtest = np.reshape(xtest, (xtest.shape[0], xtest.shape[1], 18*3))
-#     with open(output_file, 'w') as f:
-#         for i in range(len(xtrain)):
-#             min_distance = float('inf')
-#             min_index = None
-#             for j in range(len(xtest)):
-#                 distance = dtw_distance(xtrain[i, :, :], xtest[j, :, :])
-#                 f.write(f"DTW distance for generated sample {i} and true sample {j} is: {distance} \\\ \n")
-#                 if distance < min_distance:
-#                     min_distance = distance
-#                     min_index = j
-#             f.write(f"Minimal DTW distance for generated sample {i} with score {ytrain[i]:.2f} is {min_distance} of the true sample{min_index} \\\ \n")
-#             write_min_distance_info(min_distance_file, i, min_index, ytrain)
-
-
-# Example usage
-class_index =4
-# fold = 5
-# for class_index in range(0,5):
-# for fold in range(1,6):
-#     xtrain = np.load(f'../results/run_0/cross_validation/class_{class_index}/fold_{fold}/generated_samples/generated_samples_prior.npy')
-#     scores= np.load(f'../results/run_0/cross_validation/class_{class_index}/fold_{fold}/generated_samples/scores.npy').squeeze(0).squeeze(1)
-#     data= np.load(f'../results/run_0/cross_validation/class_{class_index}/fold_{fold}/generated_samples/true_samples_{class_index}.npy')
-#     print(xtrain.shape,data.shape,scores.shape)
-#     calculate_dtw_and_save_results(xtrain, data, f'../results/run_0/cross_validation/class_{class_index}/fold_{fold}/generated_samples/dtw_evaluation_results_prior.txt',scores, f'../results/run_0/cross_validation/class_{class_index}/fold_{fold}/generated_samples/min_dtw_evaluation_results_prior.txt',class_index)
-
-
-
-
-
-# calculate_dtw_for_labels = calculate_dtw_for_labels()
-# calculate_dtw_for_labels.calculate_dtw_and_save_results(xtrain,normalize_skeletons(data),f'dtw_for_label_c{class_index}.txt')
-
-
-
-
-# dtw_calculator = calculate_dtw_for_scores()
-# dtw_calculator.calculate_dtw(xtrain, data, scores, class_index)
